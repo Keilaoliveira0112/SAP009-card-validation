924ec1671973ca2adacafb4285ce87d89c7b1d32
Keila <keilaoliveira.copy@gmail.comm>
2023-01-24 21:58:33 -0300

    Reverse dos números

--- src/validator.js
+++ src/validator.js
@@ -1,19 +1,17 @@
 const validator = {
-  /* isvalid: function () {
-     console.log("validar cartão de crédito")
-   }*/
-
 
   isValid: function (campoNumeroDoCartao) {
-    const arrayCard = campoNumeroDoCartao.split("")
+    const arrayCard = campoNumeroDoCartao.split("");
+    console.log(arrayCard)
+    const arrayCardInvertido = arrayCard.reverse();
+    console.log(arrayCardInvertido)
     let somaTotal = 0;
     let resultadoDaValidacao = false;
     for (let i = 0; i < campoNumeroDoCartao.length; i++) {
-      let digito = parseInt(arrayCard[i]); // parseInt vai converter o primeiro argumento para uma string, vai analisar e retornar um inteiro ou NaN
+      let digito = parseInt(arrayCardInvertido[i]); // parseInt vai converter o primeiro argumento para uma string, vai analisar e retornar um inteiro ou NaN
 
       console.log("validar cartão de crédito" + digito)
-
-      if (i % 2 == 0) {
+      if ((i + 1) % 2 === 0) {
         digito = digito * 2;
         if (digito > 9) {
           let digitoRestante = digito % 10; // 16 % 10 = 6
